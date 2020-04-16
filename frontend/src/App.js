ceb1ab01585d93be9a9000e8d06aec8b53e51d32
davidd11 <ddoan.0411@gmail.com>
2020-04-16 13:35:04 +1000

    Create navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from './components/header';
 import LandingPage from './components/landing';
 import './App.css';
 
 function App() {
   return (
     <Router>
+      <Header/>
       <Route path="/" exact component={LandingPage}/>
     </Router>
 
---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+
+class Header extends Component {
+    render() {
+        return (
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="#home">Car Share</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                    </Nav>
+                    <Form inline>
+                        <Nav.Link href="/signup">Sign Up</Nav.Link>
+                        <Nav.Link href="/login">Log in</Nav.Link>
+                    </Form>
+                </Navbar.Collapse>
+            </Navbar>
+        )
+    }
+}
+
+export default Header;
