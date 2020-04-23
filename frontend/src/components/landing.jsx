9b40a58ac31ca5b45dc7469190402fa0332fadbf
alexwitedja <s3641938@student.rmit.edu.au>
2020-04-23 20:10:42 +1000

    added font awesome for icons and finished overview page

--- frontend/src/components/landing.jsx
+++ frontend/src/components/landing.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button } from 'react-bootstrap'
+import { Button, Container } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import '../styles/landing.css'
 import LandingBody from './landingBody'
@@ -9,19 +9,19 @@ class LandingPage extends Component {
     render() {
         return(
             <>
-            <div id="landing-page">
-                <div id="catchphrase">
-                    <h1>COOL Car Share Melbourne</h1>
-                    <h4 style={{marginBottom: '1.85rem'}}>You don't need to own a car to drive one</h4>
+                <Container id="landing-page" fluid>
+                    <div id="catchphrase">
+                        <h1>COOL Car Share Melbourne</h1>
+                        <h4>You don't need to own a car to drive one</h4>
 
-                    <Link to="/signup">
-                        <Button variant="warning">Sign Up Now</Button>
-                    </Link>
-                </div>
-            </div>
-            <div id="landing-main">
-                <LandingBody/>
-            </div>
+                        <Link to="/signup">
+                            <Button variant="warning" style={{fontSize: '2vh'}}>Sign Up Now</Button>
+                        </Link>
+                    </div>
+                </Container>
+                <Container id="landing-main" fluid>
+                    <LandingBody/>
+                </Container>
             </>
         )
     }
