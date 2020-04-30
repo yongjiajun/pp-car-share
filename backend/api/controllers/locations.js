c5a11828f408dc87b9ca419108f2650277c4084a
davidd11 <ddoan.0411@gmail.com>
2020-04-30 14:50:07 +1000

    implement create location, get all locations and get specific location api

--- backend/api/controllers/locations.js
+++ backend/api/controllers/locations.js
@@ -7,10 +7,26 @@ exports.get_all_locations = (req, res) => {
         .catch(err => res.status(400).json('Error: ') + err);
 }
 
-exports.get_location = locationid = (req, res) => {
-    res.send("hi")
+exports.get_location = (req, res) => {
+    const id = req.params.locationId;
+    Location.findById(id)
+        .then(location => res.json(location))
+        .catch(err => res.status(400).json('Error: ' + err))
 }
 
-exports.create_location = location = (req, res) => {
-    
+exports.create_location = loc = (req, res) => {
+    const req_address = req.body.address;
+    Location.findOne({ address: req_address}).then(location => {
+        if(location) {
+            return res.status(400).json({ address: "Address already exists" });
+        } else {
+            const location = new Location({
+                _id: new mongoose.Types.ObjectId(),
+                address: req.body.address
+            });
+            location.save().then(location => {
+                res.json('New location added')
+            })
+        }
+    })
 }
--- backend/api/models/location.js
+++ backend/api/models/location.js
@@ -2,10 +2,9 @@ const mongoose = require('mongoose');
 
 const locationSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    latitude: String,
-    longitude: String,
-    cars: []  // May need to update this with below
-    /* cars: [{
+    address: String
+    /*cars: []  // May need to update this with below
+     cars: [{
         car: {type: Schema.Types.ObjectId, ref: 'Car' }
     }]*/
 });
