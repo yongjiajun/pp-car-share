c77e24c45f4feb11e4f6100564148fc8eb64e098
Jia Jun Yong <yongjiajun@yahoo.com.sg>
2020-04-30 14:05:29 +1000

    added booking schema

---
+++ backend/api/models/booking.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    user: {type: Schema.Types.ObjectId, ref: "User"},
+    car: {type: Schema.Types.ObjectId, ref: "Car"},
+    bookeddatetime: { type : Date, default: Date.now },
+    pickupdatetime: Date,
+    returndatetime: Date,
+    cost: Number,
+    pickuplocation: {type: Schema.Types.ObjectId, ref: "Location"},
+    dropofflocation: {type: Schema.Types.ObjectId, ref: "Location"},
+    distance: Number,
+    status: String
+});
+
+module.exports = mongoose.model('Booking', bookingSchema);
