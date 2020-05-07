2176c52922ca327e5e4559714b11f89a0e21ec3c
Jia Jun Yong <yongjiajun@yahoo.com.sg>
2020-05-07 17:38:48 +1000

    Add cars array to locationSchema

--- backend/api/models/location.js
+++ backend/api/models/location.js
@@ -2,11 +2,8 @@ const mongoose = require('mongoose');
 
 const locationSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    address: String
-    /*cars: []  // May need to update this with below
-     cars: [{
-        car: {type: Schema.Types.ObjectId, ref: 'Car' }
-    }]*/
+    address: String,
+    cars: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Car' }]
 });
 
 module.exports = mongoose.model('Location', locationSchema);
