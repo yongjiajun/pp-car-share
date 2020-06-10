3fc09dfa12aa6be65d82aca2117d32c1497c6e22
Jia Jun Yong <yongjiajun@yahoo.com.sg>
2020-06-10 19:18:22 +1000

    add get a specific booking route for staff backend

--- backend/api/routes/bookings.js
+++ backend/api/routes/bookings.js
@@ -7,6 +7,7 @@ const BookingsController = require('../controllers/bookings');
 router.get('/customers/all', BookingsController.get_all_bookings);
 router.get('/customers/next', BookingsController.check_ready_pickup_return);
 router.get('/customers/:bookingId', BookingsController.get_user_booking);
+router.get('/:bookingId', BookingsController.get_booking)
 router.get('/', BookingsController.get_user_bookings);
 router.post('/', BookingsController.create_booking);
 router.patch('/customers/:bookingId', BookingsController.update_booking);
