48a22f56aa7aef8d17dc3d8c00761a0ff8632e51
davidd11 <ddoan.0411@gmail.com>
2020-06-10 17:27:51 +1000

    insert car model testing and update model to require fields

--- backend/api/models/car.js
+++ backend/api/models/car.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 
 const carSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    make: String,
-    seats: Number,
-    bodytype: String,
-    numberplate: String,
-    colour: String,
-    costperhour: Number,
-    fueltype: String,
-    location: {type: mongoose.Schema.Types.ObjectId, ref: "Location"},
+    make: {type: String, required: true},
+    seats: {type: Number, required: true},
+    bodytype: {type: String, required: true},
+    numberplate: {type: String, required: true},
+    colour: {type: String, required: true},
+    costperhour: {type: Number, required: true},
+    fueltype: {type: String, required: true},
+    location: {type: mongoose.Schema.Types.ObjectId, ref: "Location", required: true},
     currentbooking: {type: mongoose.Schema.Types.ObjectId, ref: "Booking"},
 });
 
