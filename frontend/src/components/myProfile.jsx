6da1ab5cec0ddeb820c516eecef1d597f2797dcc
Jia Jun Yong <yongjiajun@yahoo.com.sg>
2020-06-12 00:03:36 +1000

    fixed my profile content alignment

--- frontend/src/components/myProfile.jsx
+++ frontend/src/components/myProfile.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
 const { default: UserServiceApi } = require("../api/UserServiceApi")
 
 class MyProfilePage extends Component {
@@ -7,14 +7,14 @@ class MyProfilePage extends Component {
     render() {
         const userData = UserServiceApi.getLoggedInUserDetails();
         return (
-            <div>
+            <Container>
                 <h2>My Profile</h2>
                 <strong>First name: </strong>{userData.firstname} <br></br>
                 <strong>Last name: </strong>{userData.lastname} <br></br>
                 <strong>Email: </strong>{userData.email} <br></br>
                 <strong>Customer ID: </strong>{userData.id} <br></br>
-                <Button href='/mybookings'>My Bookings</Button>
-            </div>
+                <Button href='/mybookings'>View My Bookings</Button>
+            </Container>
         )
     }
 }
