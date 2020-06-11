e1a6a4e46d5dfaadfe02157c2644f16922cbb4e9
Jia Jun Yong <yongjiajun@yahoo.com.sg>
2020-06-12 01:28:28 +1000

    add map and car details card in view booking staff

--- frontend/src/components/staffComponents/viewBooking.jsx
+++ frontend/src/components/staffComponents/viewBooking.jsx
@@ -1,17 +1,22 @@
-import React , { Component } from 'react';
-import { Alert, Button, Container } from 'react-bootstrap';
+import React, { Component } from 'react';
+import { Alert, Button, Container, Col } from 'react-bootstrap';
 import BookingServiceApi from '../../api/BookingServiceApi';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 const { default: LocationServiceApi } = require("../../api/LocationServiceApi")
 const { default: CarServiceApi } = require("../../api/CarServiceApi")
 
-export default class ViewBookingPage extends Component {
+class ViewBookingPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
             booking: {},
             car: {},
             location: {},
-            errorMessage: ''
+            errorMessage: '',
+            activeMarker: {},
+            showingInfoWindow: false,
+            selectedPlace: {},
+            isLoading: false
         }
         this.handleCancelButton = this.handleCancelButton.bind(this);
     }
@@ -30,14 +35,42 @@ export default class ViewBookingPage extends Component {
                     })
                 LocationServiceApi.getLocationFromId(this.state.booking.location)
                     .then(res => {
-                        this.setState({
-                            location: res.data
-                        })
+                        LocationServiceApi.getGeocodeFromAddress(res.data.address)
+                            .then(newRes => {
+                                // Create object with address, latitude and longitude
+                                let locationObject = {
+                                    id: res.data._id,
+                                    address: res.data.address,
+                                    name: res.data.name,
+                                    lat: newRes.data.results[0].geometry.location.lat,
+                                    lng: newRes.data.results[0].geometry.location.lng,
+                                    cars: res.data.cars
+                                }
+                                // set new location object to react state array
+                                this.setState({
+                                    location: locationObject,
+                                    isLoading: true
+                                })
+                            });
                     })
             }).catch((error) => {
                 this.setState({ errorMessage: error.response.data.message });
             })
     }
+
+    onMarkerClick = (props, marker) =>
+        this.setState({
+            selectedPlace: props,
+            activeMarker: marker,
+            showingInfoWindow: true,
+        })
+
+    onMapClick = () =>
+        this.setState({
+            showingInfoWindow: false,
+            selectedPlace: {},
+            activeMarker: {}
+        })
 
     componentDidMount() {
         this.getBookingDetails()
@@ -61,35 +94,73 @@ export default class ViewBookingPage extends Component {
     render() {
         return (
             <Container>
-                <h2>Booking details for ID {this.state.booking._id}</h2>
+                <h2>Booking details</h2>
                 {this.state.errorMessage && <Alert variant="danger">
                     <Alert.Heading>Error obtaining booking!</Alert.Heading>
                     <p>
                         {this.state.errorMessage}
                     </p>
                 </Alert>}
+                {this.state.isLoading && <div id="garage-map" style={{ height: '400px' }}>
+                    <Map google={this.props.google}
+                        initialCenter={{
+                            lat: this.state.location.lat,
+                            lng: this.state.location.lng
+                        }}
+                        style={{ height: '400px', width: '400px' }}
+                        zoom={14}
+                        onClick={this.onMapClick}>
+
+                        <Marker
+                            id={this.state.location.id}
+                            name={this.state.location.name}
+                            address={this.state.location.address}
+                            onClick={this.onMarkerClick}
+                            position={{ lat: this.state.location.lat, lng: this.state.location.lng }}
+                        />
+
+                        <InfoWindow
+                            onClose={this.onInfoWindowClose}
+                            marker={this.state.activeMarker}
+                            visible={this.state.showingInfoWindow}>
+                            <div id="info-window">
+                                <h2>{this.state.selectedPlace.name}</h2>
+                                <p>{this.state.selectedPlace.address}</p>
+                                <a href={"/locations/" + this.state.selectedPlace.id}>Check out this location</a>
+                            </div>
+                        </InfoWindow>
+                    </Map>
+                </div>}
                 {!this.state.errorMessage &&
                     <>
-                        <b>Car ID: </b> {this.state.car._id} <br></br>
-                        <b>Car Make: </b> {this.state.car.make} <br></br>
-                        <b>Car Number Plate: </b> {this.state.car.numberplate} <br></br>
-                        <b>Car Colour: </b> {this.state.car.colour} <br></br>
-                        <b>Car Fuel Type: </b> {this.state.car.fueltype} <br></br>
-                        <b>Car Seats: </b> {this.state.car.seats} <br></br>
-                        <b>Car Body Type: </b> {this.state.car.bodytype} <br></br>
+                        <b>Booking ID: </b> {this.state.booking._id} <br></br>
                         <b>Booking time: </b> {this.state.booking.bookedtime} <br></br>
                         <b>Pickup time: </b> {this.state.booking.pickuptime} <br></br>
                         <b>Return time: </b> {this.state.booking.returntime} <br></br>
                         <b>Cost: </b> ${this.state.booking.cost} <br></br>
-                        <b>Location: </b> {this.state.location.name} <br></br>
+                        <b>Location: </b> <a href={`/admin/view/location/${this.state.location.id}`}>{this.state.location.name}</a> <br></br>
                         <b>Address: </b> {this.state.location.address} <br></br>
                         <b>Status: </b> {this.state.booking.status} <br></br>
                         {(this.state.booking.status === "Confirmed" && this.checkBookingPast(this.state.booking.pickuptime)) &&
                             <Button variant="danger" onClick={this.handleCancelButton}>Cancel</Button>
                         }
+                        <Col sm={4}>
+                            <div className="cars-div-white" style={{ 'border': 'solid black 2px' }}>
+                                <img src={this.state.car.image} alt="car" width="100" />
+                                <h2 style={{ marginTop: '1vh' }}>{this.state.car.make}</h2>
+                                <p>{this.state.car.fueltype}, {this.state.car.bodytype}, {this.state.car.seats} seaters, {this.state.car.colour}</p>
+                                <h5>${this.state.car.costperhour} per hour</h5>
+                                <h5>Number Plate: {this.state.car.numberplate}</h5>
+                                <a href={`/admin/view/cars/${this.state.car._id}`}><b>Car ID: </b>{this.state.car._id}</a>
+                            </div>
+                        </Col>
                     </>
                 }
             </Container>
         )
     }
 }
+
+export default GoogleApiWrapper({
+    apiKey: process.env.REACT_APP_API_KEY
+})(ViewBookingPage)
