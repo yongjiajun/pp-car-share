a5e5571cd875ac93fa1114e8ceae0b426c604786
Jia Jun Yong <yongjiajun@yahoo.com.sg>
2020-06-12 00:59:00 +1000

    display map and car details in card in upcoming booking

--- frontend/src/components/bookingComponents/bookingDashboard.jsx
+++ frontend/src/components/bookingComponents/bookingDashboard.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Form, Col, Button, Row, Alert, Container } from 'react-bootstrap';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 import CarServiceApi from '../../api/CarServiceApi';
 import BookingServiceApi from '../../api/BookingServiceApi';
 import LocationServiceApi from '../../api/LocationServiceApi';
@@ -17,7 +18,11 @@ class BookingDashboard extends Component {
             location: '',
             successMsg: '',
             availablePickup: false,
-            avaialbleReturn: false
+            avaialbleReturn: false,
+            activeMarker: {},
+            showingInfoWindow: false,
+            selectedPlace: {},
+            isLoading: false
         }
         this.handleSubmit = this.handleSubmit.bind(this)
         this.handleChange = this.handleChange.bind(this)
@@ -57,6 +62,20 @@ class BookingDashboard extends Component {
             })
     }
 
+    onMarkerClick = (props, marker) =>
+    this.setState({
+        selectedPlace: props,
+        activeMarker: marker,
+        showingInfoWindow: true,
+    })
+
+onMapClick = () =>
+    this.setState({
+        showingInfoWindow: false,
+        selectedPlace: {},
+        activeMarker: {}
+    })
+
     handleReturnButton() {
         this.state.nextBooking.status = 'Returned';
         this.state.nextBooking.id = this.state.nextBooking._id;
@@ -88,9 +107,23 @@ class BookingDashboard extends Component {
                     })
                 LocationServiceApi.getLocationFromId(res.data.location)
                     .then(res => {
-                        this.setState({
-                            location: res.data
-                        })
+                        LocationServiceApi.getGeocodeFromAddress(res.data.address)
+                            .then(newRes => {
+                                // Create object with address, latitude and longitude
+                                let locationObject = {
+                                    id: res.data._id,
+                                    address: res.data.address,
+                                    name: res.data.name,
+                                    lat: newRes.data.results[0].geometry.location.lat,
+                                    lng: newRes.data.results[0].geometry.location.lng,
+                                    cars: res.data.cars
+                                }
+                                // set new location object to react state array
+                                this.setState({
+                                    location: locationObject,
+                                    isLoading: true
+                                })
+                            });
                     })
             }
         }).catch((error) => {
@@ -112,13 +145,37 @@ class BookingDashboard extends Component {
                 {this.state.nextBookingExists &&
                     <div className="white-cards-div">
                         <Container>
-                            <h2>Your upcoming booking: </h2>
-                            <b>Car Make: </b> {this.state.car.make} <br></br>
-                            <b>Car Number Plate: </b> {this.state.car.numberplate} <br></br>
-                            <b>Car Colour: </b> {this.state.car.colour} <br></br>
-                            <b>Car Fuel Type: </b> {this.state.car.fueltype} <br></br>
-                            <b>Car Seats: </b> {this.state.car.seats} <br></br>
-                            <b>Car Body Type: </b> {this.state.car.bodytype} <br></br>
+                        <h2>Your upcoming booking: </h2>
+                        {this.state.isLoading && <div id="garage-map" style={{ height: '400px' }}>
+                            <Map google={this.props.google}
+                                initialCenter={{
+                                    lat: this.state.location.lat,
+                                    lng: this.state.location.lng
+                                }}
+                                style={{ height: '400px', width: '400px' }}
+                                zoom={14}
+                                onClick={this.onMapClick}>
+
+                                <Marker
+                                    id={this.state.location.id}
+                                    name={this.state.location.name}
+                                    address={this.state.location.address}
+                                    onClick={this.onMarkerClick}
+                                    position={{ lat: this.state.location.lat, lng: this.state.location.lng }}
+                                />
+
+                                <InfoWindow
+                                    onClose={this.onInfoWindowClose}
+                                    marker={this.state.activeMarker}
+                                    visible={this.state.showingInfoWindow}>
+                                    <div id="info-window">
+                                        <h2>{this.state.selectedPlace.name}</h2>
+                                        <p>{this.state.selectedPlace.address}</p>
+                                        <a href={"/locations/" + this.state.selectedPlace.id}>Check out this location</a>
+                                    </div>
+                                </InfoWindow>
+                            </Map>
+                        </div>}
                             <b>Booking time: </b> {this.state.nextBooking.bookedtime} <br></br>
                             <b>Pickup time: </b> {this.state.nextBooking.pickuptime} <br></br>
                             <b>Return time: </b> {this.state.nextBooking.returntime} <br></br>
@@ -128,6 +185,15 @@ class BookingDashboard extends Component {
                             <b>Status: </b> {this.state.nextBooking.status} <br></br>
                             <Button variant="success" onClick={this.handlePickupButton} disabled={!this.state.availablePickup}>Pickup</Button>
                             <Button variant="danger" onClick={this.handleReturnButton} disabled={!this.state.avaialbleReturn}>Return</Button>
+                            <Col sm={4}>
+                                <div className="cars-div-white" style={{ 'border': 'solid black 2px' }}>
+                                    <img src={this.state.car.image} alt="car" width="100" />
+                                    <h2 style={{ marginTop: '1vh' }}>{this.state.car.make}</h2>
+                                    <p>{this.state.car.fueltype}, {this.state.car.bodytype}, {this.state.car.seats} seaters, {this.state.car.colour}</p>
+                                    <h5>Number Plate: {this.state.car.numberplate}</h5>
+                                    <p><b>Car ID: </b>{this.state.car._id}</p>
+                                </div>
+                        </Col>
                         </Container>
                     </div>
                 }
@@ -168,4 +234,6 @@ class BookingDashboard extends Component {
     }
 }
 
-export default BookingDashboard;
+export default GoogleApiWrapper({
+    apiKey: process.env.REACT_APP_API_KEY
+})(BookingDashboard)
