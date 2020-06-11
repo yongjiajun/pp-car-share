ee885cc510f1a26d0e217b9d30f4c9e2cdbccb56
Jia Jun Yong <yongjiajun@yahoo.com.sg>
2020-06-12 00:45:01 +1000

    added contact details in footer

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -7,7 +7,9 @@ export default class footer extends Component {
         return (
             <div className="footer-parent">
                 <div className="footer">
-                    <p>Email us: Gory@Rover.com</p>
+                    <p>Email us: <a href="mailto:mza@carshare.com">mza@carshare.com</a></p>
+                    <p>Call us: (03) 1234 5678</p>
+                    <p>Visit us: MZA Car Share, 124 La Trobe St, Melbourne VIC 3000</p>
                 </div>
             </div>
         )
